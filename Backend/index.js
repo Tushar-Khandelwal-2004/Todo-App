34e7bd465ca4fafcfd34db07b287bb7b1452f892
Tushar-Khandelwal-2004 <khandelwal.tushar8527@gmail.com>
2024-12-31 22:48:27 +0530

    Initialised Frontend and added CORS in Backend

--- Backend/index.js
+++ Backend/index.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const cors=require("cors");
 require("dotenv").config();
 const app = express();
 app.use(express.json());
+app.use(cors());
 const {todoRouter}=require("./routes/todo")
 app.use("/todo",todoRouter);
 async function main() {
