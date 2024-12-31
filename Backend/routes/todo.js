2820d1e0cd661ef5fc5bc319151b02d4692a0bc9
Tushar-Khandelwal-2004 <khandelwal.tushar8527@gmail.com>
2024-12-31 23:36:20 +0530

    Fixed get and delete routes

--- Backend/routes/todo.js
+++ Backend/routes/todo.js
@@ -3,29 +3,76 @@ const router = Router();
 const { todoModel } = require("../db");
 router.get("/", async function (req, res) {
     try {
-        const todo = await todoModel.find().select("title -_id");
-        // console.log(todo);
-        const todos=todo.map(todo=>todo.title);
+        const todos = await todoModel.find().select("title _id"); 
+        const formattedTodos = todos.map(todo => ({
+            id: todo._id,  
+            title: todo.title 
+        }));
         res.json({
-            todos
-        })
-    }
-    catch{ 
+            todos: formattedTodos  
+        });
+    } catch {
         res.status(403).send({
-            message:"Todo get failed!"
-        })
+            message: "Todo get failed!"
+        });
     }
 });
-router.post("/addTodo",async function(req,res) {
-    const {title}=req.body;
+router.post("/addTodo", async function (req, res) {
+    const { title } = req.body;
     await todoModel.create({
-        title:title
+        title: title
     });
     res.json({
-        message:"Todo Created!"
+        message: "Todo Created!"
     })
 });
 
+router.delete("/deleteTodo", async function (req, res) {
+    const id = req.headers.id;  // Get the ID from headers
+    console.log("Deleting Todo with ID:", id);
+    try {
+        const result = await todoModel.findByIdAndDelete(id);
+        console.log(result);
+        console.log(!result);
+        if (!result) {
+            return res.status(404).json({
+                message: "Todo not found!",
+            });
+        }
+
+        res.json({
+            message: "Todo Deleted!",
+        });
+
+    } catch (e) {
+        res.json({
+            message: "Error Deleting Todo"
+        })
+    }
+
+    // const id = req.params.id;
+    // console.log("Deleting Todo with ID:", id);
+    // console.log(id);
+    // try {
+    //     const result = await todoModel.findByIdAndDelete(id);
+    //     if (!result) {
+    //         return res.json({
+    //             message: "Todo not found!"
+    //         })
+    //     }
+    //     else {
+    //         return res.json({
+    //             message: "Todo Deleted!"
+    //         })
+    //     }
+    // }
+    // catch(e){
+    //     return res.json({
+    //         message:"Error Deleting Todo"
+    //     })
+    // }
+
+})
 
 module.exports = {
     todoRouter: router
